729f8b498ce22ce590ae4b20f21f46e7282c5f36
FandomChild <martinezlilia101@gmail.com>
2024-02-22 18:42:57 -0800

    have home page with explanation and app.post for dates

--- index.js
+++ index.js
@@ -5,22 +5,60 @@ import bodyParser from "body-parser";
 const app = express();
 const port = 3000;
 const URL = "https://api.nasa.gov/planetary/apod"
-const apiKey = "ryIfDBX9yydJB2rZTTXcALzDNebsTGHIh64kG6Q6"; 
+const apiKey = "ryIfDBX9yydJB2rZTTXcALzDNebsTGHIh64kG6Q6";
+var copyright;
 
 app.use(express.static("public"));
-app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.urlencoded({ extended: true }));
+
+
+// get preferred picture from API that doesn't have copyright
 
 app.get("/", async (req, res) => {
     try {
         const result = await axios.get(`${URL}?`, {
             params: {
                 api_key: apiKey,
+                date: '2014-11-17',
               }
         });
-        console.log(result.data.url);
-        var nasaImg = result.data.url;
+        var key = result.data
+
         res.render("index.ejs", {
-            adop: nasaImg,
+            my: key,
+
+        });
+    } catch (error) {
+        console.log(error.response.data);
+        res.status(500);
+    }
+});
+
+app.post("/adop", async (req, res) => {
+
+    console.log(req.body["date"]);
+    var calDate = req.body["date"];
+    try {
+        const result = await axios.get(`${URL}?`, {
+            params: {
+                api_key: apiKey,
+                date: calDate,
+              }
+        });
+
+        var key = result.data
+
+        if (!key.copyright) {
+            console.log("NASA");
+            copyright = "NASA"
+        } else {
+            copyright = "copyright"
+        }
+
+        res.render("adop.ejs", {
+            log: key,
+            mssg: copyright,
+
         });
     } catch (error) {
         console.log(error.response.data);
@@ -31,3 +69,4 @@ app.get("/", async (req, res) => {
 app.listen(port, () => {
     console.log(`Listening on port ${port}`);
   });
+
